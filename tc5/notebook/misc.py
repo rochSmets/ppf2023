32196107f8642f1e591d4cd6a00ae6051aa42aa0
roch smets <roch.smets@gmail.com>
2023-10-24 20:47:29 +0200

    wip

--- tc5/notebook/misc.py
+++ tc5/notebook/misc.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 
-
-
 # sc functions needed to settle a numerical scheme
 # ============================================
-
 
 
 import numpy as np
 
 
-def addGhosts(x):
-    x_wide = np.zeros(x.shape[0]+2)
-    x_wide[1:-1] = x
+def addGhosts(x, num_of_ghosts=1):
+    x_wide = np.ones(x.shape[0]+2*num_of_ghosts)*(-1)
+    x_wide[num_of_ghosts:-num_of_ghosts] = x
     return x_wide
 
-def fillGhosts(x, periodic = True):
-    if periodic :
-        x[0] = x[-2]
-        x[-1] = x[1]
-    return x
 
+def fillGhosts(x, num_of_ghosts=1, periodic=True):
+    if periodic:
+        x[:num_of_ghosts] = x[-2*num_of_ghosts:-num_of_ghosts]
+        x[-num_of_ghosts:] = x[num_of_ghosts:2*num_of_ghosts]
+    return x
